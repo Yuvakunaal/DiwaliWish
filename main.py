ff6b1ece5221ff6cc825f18b0b2c1f6eff8db766
Yuvakunaal <126342173+Yuvakunaal@users.noreply.github.com>
2023-11-12 00:54:09 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+import streamlit as st
+
+
+# with open("Animation - 1699723958293.json") as source :
+#     animation1 = json.load(source)
+# st_lottie(animation1,speed=1.5,height=0,width=375)
+
+st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6-1w6SCu61oBwCziK7KYo4qHtsZpajqgwzg&usqp=CAU", use_column_width=True)
+st.markdown('''
+<!DOCTYPE html>
+<html>
+<head>
+<br>
+<h6 class=head>Wish you all a very</h6>
+<h1 class="heading1">🧨 Happy Diwali 🧨</h1>
+<br>
+<h4 class="heading2">-- Kunaal</h4>
+<style>
+        body {
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+        }
+        .background {
+            position: fixed;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+        }
+        .st-emotion-cache-6q9sum
+        {
+            visibility : hidden;
+        }
+        .st-emotion-cache-cio0dv
+        {
+            visibility : hidden;
+        }
+        .st-emotion-cache-1wbqy5l
+        {
+            visibility : hidden;
+        }
+        .heading1
+        {
+            text-align : center;
+        }
+        .heading2
+        {
+            text-align : right;
+        }
+        .head
+        {
+            text-align : center;
+        }
+</style>
+</head>
+</html>
+
+''',unsafe_allow_html=True)
+
+
+
+
+
+
